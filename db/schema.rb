a7527591c6fec86d97a7e9c956bf13a8062f03a4
KChinchillaHN <kchinchillahn@hotmail.com>
2017-02-05 17:52:52 -0600

    New Field - work_id in Employees

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170205230530) do
+ActiveRecord::Schema.define(version: 20170205234430) do
 
   create_table "employees", force: :cascade do |t|
     t.string   "name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20170205230530) do
     t.integer  "emergency_contact_number"
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.integer  "work_id"
   end
 
   create_table "users", force: :cascade do |t|
