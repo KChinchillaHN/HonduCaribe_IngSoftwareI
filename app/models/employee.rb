8d3128bcf436be0192c833aec6b7c796e5cc32ff
KChinchillaHN <kchinchillahn@hotmail.com>
2017-02-08 16:34:49 -0600

    Login _ Begin

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,7 @@
 class Employee < ActiveRecord::Base
+	validates_uniqueness_of :work_id, :name 
+	validates_numericality_of :work_id, greater_than:0
+	validates_numericality_of :number_id, greater_than:0
+	validates_numericality_of :emergency_contact_number, greater_than:0
+
 end
