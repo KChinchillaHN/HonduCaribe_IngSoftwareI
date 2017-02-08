5f6ca010a63c96221ae46aa28438cb85b7e12e97
JonathanS96 <jonathansalazar96@gmail.com>
2017-02-08 14:22:04 -0600

    Almost ready #1

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,10 @@
 class Employee < ActiveRecord::Base
+
+  def self.civil_statuses
+    ["Soltero", "Casado", "Viudo", "Divorciado", "Union Libre"];
+  end
+
+  def self.bloods
+    ["O-", "O" , "A-", " A+" ,"B-", " B+" , "AB-", " AB+"]
+  end
 end
