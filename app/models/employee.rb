9c21822ef67813e4a2d8fb787d70e127e748c843
KChinchillaHN <kchinchillahn@hotmail.com>
2017-02-08 16:54:16 -0600

    Corrections1_Merge
    Correction 1

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,11 +1,8 @@
 class Employee < ActiveRecord::Base
-<<<<<<< HEAD
 	validates_uniqueness_of :work_id, :name 
 	validates_numericality_of :work_id, greater_than:0
 	validates_numericality_of :number_id, greater_than:0
 	validates_numericality_of :emergency_contact_number, greater_than:0
-
-=======
 
   def self.civil_statuses
     ["Soltero", "Casado", "Viudo", "Divorciado", "Union Libre"];
@@ -14,5 +11,4 @@ class Employee < ActiveRecord::Base
   def self.bloods
     ["O-", "O" , "A-", " A+" ,"B-", " B+" , "AB-", " AB+"]
   end
->>>>>>> 5f6ca010a63c96221ae46aa28438cb85b7e12e97
 end
