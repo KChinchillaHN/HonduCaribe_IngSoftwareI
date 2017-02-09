2ddf6b3e911b2d8bce454dcb9e6a0881fc3b42b6
KChinchillaHN <kchinchillahn@hotmail.com>
2017-02-08 23:01:15 -0600

    Login Module
    Module of Login finish, Pending of tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+	def nice_date(fecha)
+		return nil unless fecha
+		fecha.strftime("%A, %d de %B de %Y")
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   resources :employees
+  resources :users
   get 'test', to: "employees#test"
   root 'plainpage#index'
 
