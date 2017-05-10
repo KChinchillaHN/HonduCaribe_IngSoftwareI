03b3321f2a491567832fcdd5f0cc2c63ff18cfc6
vagrant <vagrant@precise32.(none)>
2017-05-10 16:52:17 +0000

    Fixed some UI stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170509172331) do
+ActiveRecord::Schema.define(version: 20170510043831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -171,6 +171,7 @@ ActiveRecord::Schema.define(version: 20170509172331) do
     t.integer  "instructor_id"
     t.integer  "institution_id"
     t.boolean  "planned"
+    t.string   "category"
   end
 
   create_table "users", force: :cascade do |t|
